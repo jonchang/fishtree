cf12b67ab3a5b4c17c4eb3f6c0fe5f2c85430f66
Jonathan Chang <jchang641@gmail.com>
2019-02-19 17:31:47 +1100

    Add documentation about the package itself

---
+++ R/fishtree.R
@@ -0,0 +1,13 @@
+#' fishtree: Interface to the Fish Tree of Life API
+#'
+#' fishtree makes it easy to access phylogenetic information about ray-finned
+#' fishes with R. Download taxonomies, phylogenies, sequence matrices, and
+#' diversification rate data with a simple set of functions.
+#'
+#' Implementation note: this package makes calls over the network and caches
+#' its (sometimes large) results for faster loading. Because of this,
+#' long-running R instances could use a lot of memory.
+#'
+#' @docType package
+#' @name fishtree-package
+NULL
