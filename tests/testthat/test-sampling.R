d3ecab8b82e2c3b28bedcf58e677d6ae45ac0470
Jonathan Chang <jchang641@gmail.com>
2019-04-02 18:12:53 +1100

    Update tests

--- tests/testthat/test-sampling.R
+++ tests/testthat/test-sampling.R
@@ -1,10 +1,13 @@
 context("test-sampling")
 
 test_that("phylogeny works", {
-  spp <- c("Acanthurus achilles","Acanthurus auranticavus","Acanthurus bahianus", "Nonexistant")
+  spp <- c("Acanthurus achilles","Acanthurus auranticavus","Acanthurus bahianus")
   expect_equal(3, ape::Ntip(fishtree_phylogeny(species = spp)))
-  expect_warning(fishtree_phylogeny(species = spp), "Missing names")
   expect_is(fishtree_phylogeny(species = spp), "phylo")
 })
 
+test_that("warn on extra species", {
+  spp <- c("Acanthurus achilles","Acanthurus auranticavus","Acanthurus bahianus", "Nonexistant")
+  expect_warning(fishtree_phylogeny(species = spp), "Missing names")
+})
 
