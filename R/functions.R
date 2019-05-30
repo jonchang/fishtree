6005d87488bf6dd4e866975fe0d26afda9e9eb67
Jonathan Chang <jchang641@gmail.com>
2019-05-30 13:07:06 -0700

    Update docs for fishtree_phylogeny

--- R/functions.R
+++ R/functions.R
@@ -11,9 +11,15 @@
 #' @return An object of class `"phylo"`.
 #' @references Rabosky, D. L., Chang, J., Title, P. O., Cowman, P. F., Sallan, L., Friedman, M., Kashner, K., Garilao, C., Near, T. J., Coll, M., Alfaro, M. E. (2018). An inverse latitudinal gradient in speciation rate for marine fishes. Nature, 559(7714), 392–395. doi:10.1038/s41586-018-0273-1
 #' @examples
+#' # Get a phylogeny for a taxonomic rank
 #' surgeons <- fishtree_phylogeny(rank = "Acanthuridae")
 #'
+#' # Get a phylogeny for only certain species
+#' genomic_fish <- c("Oryzias latipes", "Tetraodon nigroviridis", "Gasterosteus aculeatus", "Danio rerio")
+#' fishtree_phylogeny(species = genomic_fish)
+#'
 #' # Chronograms may not be ultrametric due to numerical precision issues
+#' # Consider using phytools::force.ultrametric
 #' ape::is.ultrametric(surgeons)
 #' ape::is.ultrametric(surgeons, tol = 0.00001)
 #'
