472fe995958f0a477114489c39d086ab44005c5c
Jonathan Chang <jchang641@gmail.com>
2019-12-17 15:38:28 +1100

    Skip all tests if offline

--- tests/testthat/test-sampling.R
+++ tests/testthat/test-sampling.R
@@ -1,19 +1,22 @@
 context("test-sampling")
 
 test_that("phylogeny works", {
+  skip_if_offline()
   spp <- c("Acanthurus achilles","Acanthurus auranticavus","Acanthurus bahianus")
   expect_equal(3, ape::Ntip(fishtree_phylogeny(species = spp)))
   expect_is(fishtree_phylogeny(species = spp), "phylo")
 })
 
 test_that("warn on extra species", {
+  skip_if_offline()
   spp <- c("Acanthurus achilles","Acanthurus auranticavus","Acanthurus bahianus", "Nonexistant")
   expect_warning(fishtree_phylogeny(species = spp), "only found 3 species")
 })
 
 
 test_that("alignment single species works", {
-    skip_on_cran() # Takes a long time
-    spp <- "Abalistes_stellaris"
-    expect_s3_class(fishtree_alignment(species = spp), "DNAbin")
+  skip_if_offline()
+  skip_on_cran() # Takes a long time
+  spp <- "Abalistes_stellaris"
+  expect_s3_class(fishtree_alignment(species = spp), "DNAbin")
 })
